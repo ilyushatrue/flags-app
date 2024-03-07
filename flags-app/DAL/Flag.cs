ec5f695d95e404b2c0326d608489b34dfefc897d
itruhin <i.truhin@cmachine.ru>
2024-03-07 17:14:02 +1000

    initial

---
+++ flags-app/DAL/Flag.cs
@@ -0,0 +1,10 @@
+﻿namespace DAL;
+
+internal class Flag
+{
+    public int Id { get; set; }
+    public FlagColorEnum Color { get; set; }
+    public bool HasPattern { get; set; }
+    public int MyProperty { get; set; }
+
+}
